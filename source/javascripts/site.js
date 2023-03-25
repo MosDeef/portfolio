2d9f458a21d49d7fdfa3dd766ad5994da73837e3
Jim Deeth <james.deeth@gmail.com>
2023-03-25 12:06:05 +0900

    font change

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+import { annotate } from 'rough-notation';
+const title = document.querySelector('.title');
+const roughNotation = document.querySelector('.rough-notation');
+const link = document.querySelector('.nav-link');
+const list = document.querySelector('.list');
+const annotation = annotate(title, { type: 'highlight' , color:'pink'});
+annotation.show();
