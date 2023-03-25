d7df8e25441db9c2cf2fd19acd111b15b275bdfd
Jim Deeth <james.deeth@gmail.com>
2023-03-25 15:02:16 +0900

    rough notation with error

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,7 +1,7 @@
-import { annotate } from 'rough-notation';
+import { annotate } from "https://unpkg.com/rough-notation?module";
 const title = document.querySelector('.title');
 const roughNotation = document.querySelector('.rough-notation');
 const link = document.querySelector('.nav-link');
 const list = document.querySelector('.list');
-const annotation = annotate(title, { type: 'highlight' , color:'pink'});
+const annotation = annotate(title, { type: 'highlight' , color:'blue'});
 annotation.show();
